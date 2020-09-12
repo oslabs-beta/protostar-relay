bb22f764aa2775e7478c612e8afa6521460d719a
marcburnie <63939269+marcburnie@users.noreply.github.com>
2020-09-12 14:10:13 -0500

    cleaned up network - merged transactions with same id and split by transaction type. Menu filters work on type and id

--- src/devtools/view/NetworkDisplayer.js
+++ src/devtools/view/NetworkDisplayer.js
@@ -2,11 +2,38 @@ import React, { useState, useEffect, useContext } from "react";
 import { StoreContext } from '../context';
 import Record from './Components/Record';
 import { v4 as uuidv4 } from "uuid";
+import { execute } from "graphql";
 
-  const createUUIDs = events => events.map((event) => {
-      return { ...event, __id: uuidv4() };
-    });
+const createUUIDs = events => events.map((event) => {
+  return { ...event, __id: uuidv4() };
+});
 
+const combineEvents = (events) => {
+  const combinedEvents = {};
+  const eventTypes = {};
+  events.forEach(event => {
+    const tempObj = {};
+    if (event.name === "execute.start") {
+      tempObj.request = event.params;
+      tempObj.variables = event.variables;
+    } else if (event.name === "execute.info") {
+      tempObj.info = event.info
+    } else if (event.name === "execute.next") {
+      tempObj.response = event.response
+    } else if (event.name === "execute.complete") {
+      tempObj.complete = true
+    }
+    combinedEvents[event.transactionID] ? combinedEvents[event.transactionID] = Object.assign(combinedEvents[event.transactionID], tempObj) : combinedEvents[event.transactionID] = tempObj;
+  })
+
+  //sort by type
+  Object.keys(combinedEvents).forEach(transactionID => {
+    const op = combinedEvents[transactionID].request.operationKind;
+    eventTypes[op] ? eventTypes[op] = Object.assign(eventTypes[op], { [transactionID]: combinedEvents[transactionID] }) : eventTypes[op] = { [transactionID]: combinedEvents[transactionID] }
+  })
+
+  return eventTypes;
+}
 
 
 const NetworkDisplayer = (props) => {
@@ -22,50 +49,57 @@ const NetworkDisplayer = (props) => {
     };
     store.addListener('mutated', onMutated);
 
-    setEvents(createUUIDs(store._environmentEventsMap.get(1) || []));
+    setEvents(combineEvents(store._environmentEventsMap.get(1) || []));
 
     return () => {
       store.removeListener('mutated', onMutated);
     };
   }, [store]);
 
-      //handle type menu click events
-    function handleIdClick(e, id) {
-        //set new selection
-        setSelection(id);
+  //handle type menu click events
+  function handleMenuClick(e, id) {
+    //set new selection
+    setSelection(id);
+  }
+
+  //shows you the entire network
+  function handleReset(e) {
+    //force update
+    setEvents(combineEvents(store._environmentEventsMap.get(1) || []));
+    //remove selecti on;
+    setSelection("");
+  };
+
+  const eventMenu = [];
+  const eventsList = [];
+  //for each event - add to menu list
+  for (let type in events) {
+
+    //creates an array of menu items for all events belonging to a given type
+    const typeList = [];
+    for (let id in events[type]) {
+      typeList.push(
+        <li>
+          <a id={id} className={(selection === (id)) && "is-active"} onClick={(e) => { handleMenuClick(e, id) }}>{events[type][id].request.name}</a>
+        </li>)
+      //creates an array of elements for all events
+      eventsList.push(
+        <div id={id} className={`${(selection !== id && selection !== type && selection !== "") ? "is-hidden" : "record-line"}`}>
+          <Record {...events[type][id]} />
+        </div>
+      );
     }
 
-    //shows you the entire network
-    function handleReset(e) {
-      //force update
-      setEvents(createUUIDs(store._environmentEventsMap.get(1) || []));
-        //remove selecti on;
-        setSelection("");
-    };
-
-    console.log("store",store)
-    
-    const eventMenu = [];
-    const eventsList = []
-
-    console.log("events", events)
-
-    //for each event - add to menu list
-    events.forEach(event => {
-      eventMenu.push(
-          <li>
-            <a id={event.__id} className={(selection === (event.__id)) && "is-active"} onClick={(e) => {handleIdClick(e, event.__id)}}>{event.name}</a>
-        </li>
-      );
-
-      console.log(event.name, event.__id, selection, ((selection !== (event.__id)) && selection !== ""))
-
-        eventsList.push(
-          <div id={event.__id} className={`record-line ${((selection !== (event.__id)) && selection !== "") && "is-hidden"}`}>
-            <Record {...event} />
-          </div>
-        );
-    })
+    //pushes the new type element with child events to the typeList component array
+    eventMenu.push(
+      <li>
+        <a id={type} className={(selection === type) && "is-active"} onClick={(e) => { handleMenuClick(e, type) }}>
+          {type}
+        </a>
+        <ul>{typeList}</ul>
+      </li>
+    );
+  }
 
 
 
--- src/devtools/view/StoreDisplayer.js
+++ src/devtools/view/StoreDisplayer.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Record from './Components/Record';
 
-const StoreDisplayer = ({store}) => {
-    const [recordsList, setRecordsList] = useState({});
-    const [selection, setSelection] = useState("");
+const StoreDisplayer = ({ store }) => {
+  const [recordsList, setRecordsList] = useState({});
+  const [selection, setSelection] = useState("");
 
   //update record list to current selection
-  function updateRecords (selection) {
+  function updateRecords(selection) {
     console.log("selection", selection)
     if (selection === "") {
       setRecordsList(store);
-    //id selected - filter out everything except selected id
+      //id selected - filter out everything except selected id
     } else if (selection[0] === "i") {
       const id = selection.slice(3)
       setRecordsList(
@@ -36,82 +36,70 @@ const StoreDisplayer = ({store}) => {
     updateRecords("");
   }, [store]);
 
-    //handle menu click events
-     function handleMenuClick(e, selection) {
-      //set new selection
-      setSelection(selection);
-      //update display with current selection
-      updateRecords(selection);
-    }
-    // //handle type menu click events
-    // function handleIdClick(e, id) {
-    //     //set new selection
-    //     setSelection("id-"+id);
-    //     //update record list to current selection
-    //     setRecordsList(
-    //       Object.keys(store).reduce((newRL, key) => {
-    //           if (store[key].__id === id) newRL[key] = store[key];
-    //         return newRL;
-    //       }, {})
-    //     );
-    // }
+  //handle menu click events
+  function handleMenuClick(e, selection) {
+    //set new selection
+    setSelection(selection);
+    //update display with current selection
+    updateRecords(selection);
+  }
 
-    //shows you the entire store
-    function handleReset(e) {
-        //remove selection
-        setSelection("");
-        //reset back to original store
-        updateRecords("");
-    }
+  //shows you the entire store
+  function handleReset(e) {
+    //remove selection
+    setSelection("");
+    //reset back to original store
+    updateRecords("");
+  }
 
-    //create menu list of all types
-    const menuList = {}
-    for (let id in store) {
-        const record = store[id];
-        menuList[record.__typename] ? menuList[record.__typename].push(record.__id) : menuList[record.__typename] = [record.__id];
-    }
+  //create menu list of all types
+  const menuList = {}
+  for (let id in store) {
+    const record = store[id];
+    menuList[record.__typename] ? menuList[record.__typename].push(record.__id) : menuList[record.__typename] = [record.__id];
+  }
 
-    //loop through each type and generate menu item
-    const typeList = [];
-    for (let type in menuList) {
-        //creates an array of elements for all ids belonging to a given type
-        const idList = menuList[type].map(id => {
-            return (
-                <li>
-                    <a id={"id-" + id} className={(selection === ("id-"+id)) && "is-active"} onClick={(e) => {handleMenuClick(e, ("id-" + id))}}>{id}</a>
-                </li>
-            )
-        })
-        //pushes the new type element with child ids to the typeList component array
-        typeList.push(
-          <li>
-            <a id={"type-" + type} className={(selection === ("type-"+type)) && "is-active"} onClick={(e) => {handleMenuClick(e, ("type-" + type))}}>
-              {type}
-            </a>
-            <ul>{idList}</ul>
-          </li>
-        );
-    }
-    return (
-        <React.Fragment>
-            <div className="column">
-                <button className="button is-small is-link" onClick={(e)=>{handleReset(e)}}>Reset</button>
-                <aside className="menu">
-                <p className="menu-label">
-                    Record List
-                </p>
-                <ul className="menu-list">
-                    {typeList}
-                </ul>
-                </aside>
-            </div>
-            <div className="column">
-                <div className="display-box">
-                    <Record {...recordsList} />
-                </div>
-          </div>
-        </React.Fragment>
+  //loop through each type and generate menu item
+  const typeList = [];
+  for (let type in menuList) {
+    //creates an array of elements for all ids belonging to a given type
+    const idList = menuList[type].map(id => {
+      return (
+        <li>
+          <a id={"id-" + id} className={(selection === ("id-" + id)) && "is-active"} onClick={(e) => { handleMenuClick(e, ("id-" + id)) }}>{id}</a>
+        </li>
+      )
+    })
+    //pushes the new type element with child ids to the typeList component array
+    typeList.push(
+      <li>
+        <a id={"type-" + type} className={(selection === ("type-" + type)) && "is-active"} onClick={(e) => { handleMenuClick(e, ("type-" + type)) }}>
+          {type}
+        </a>
+        <ul>{idList}</ul>
+      </li>
     );
+  }
+  return (
+    <React.Fragment>
+      <div className="column">
+        <button className="button is-small is-link" onClick={(e) => { handleReset(e) }}>Reset</button>
+        <aside className="menu">
+          <p className="menu-label">
+            Record List
+                </p>
+          <ul className="menu-list">
+            {typeList}
+          </ul>
+        </aside>
+      </div>
+      <div className="column">
+        <div className="display-box">
+          <Record {...recordsList} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default StoreDisplayer;
